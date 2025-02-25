c4240c148ef028e3471a9ff61ac99ed874dca7d4
Arseniy Shablyko <241208arseniy@gmail.com>
2025-02-25 18:51:00 +0300

    Add test05

--- Task01/test04.cpp
+++ Task01/test04.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int number = 100;
-	int* pointer = &number;
-
-	cout << "Before number = " << number << endl;
-
-	*pointer = *pointer * 5;
-
-	cout << "After number = " << number << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int number = 100;
+//	int* pointer = &number;
+//
+//	cout << "Before number = " << number << endl;
+//
+//	*pointer = *pointer * 5;
+//
+//	cout << "After number = " << number << endl;
+//
+//	return 0;
+//}
