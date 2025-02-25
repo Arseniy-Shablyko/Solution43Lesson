8ee7617e73202397373248a635679db85f7ecf27
Arseniy Shablyko <241208arseniy@gmail.com>
2025-02-25 18:07:26 +0300

    Add project files.

---
+++ Task01/test01.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int b;
+//
+//void test01() {
+//	auto a = 12;
+//	register int a;
+//}
+//
+//void test02() {
+//	// register, auto, static, extern
+//	auto c = 12;
+//}
+//
+//int main() {
+//	test01();
+//	int a;
+//
+//	return 0;
+//}
---
+++ Task01/test02.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	char valueChar;
+//	short int valueShort;
+//	int valueInt;
+//	double valueDouble;
+//
+//	cout << "Size of char: " << sizeof(valueChar) << endl;
+//	cout << "Size of short int: " << sizeof(valueShort) << endl;
+//	cout << "Size of int: " << sizeof(valueInt) << endl;
+//	cout << "Size of double: " << sizeof(valueDouble) << endl;
+//
+//	cout << "\n";
+//
+//	char* ptrChar;
+//	short* ptrShort;
+//	int ptrInt;
+//	double* ptrDouble;
+//
+//	cout << "Size of char pointer: " << sizeof(ptrChar) << endl;
+//	cout << "Size of short int pointer: " << sizeof(ptrShort) << endl;
+//	cout << "Size of int pointer: " << sizeof(ptrInt) << endl;
+//	cout << "Size of double pointer: " << sizeof(ptrDouble) << endl;
+//	
+//	return 0;
+//}
